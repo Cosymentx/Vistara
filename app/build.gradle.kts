409ca31803489aeb60671ed5197a53aec9a4770e
Waiting <waitinghc@foxmail.com>
2026-03-12 09:59:39 +0800

    feat: 版本升级

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.obscura.wallpapers"
         minSdk = 25
         targetSdk = 36
-        versionCode = 5
+        versionCode = 8
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
